ab2b405099d95c494e57756ad05f1a1a722f4771
Aiko <tysaiw@gmail.com>
2023-10-02 22:07:31 +0800

    Add views and update controller actions
    - Add views for Book index and show pages
    - Update index and show actions in Books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find_by(params[:id])
+    puts "Found book: #{@book.inspect}"
+    @reviews = @book.reviews
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root "books#index"
-  get 'books/show'
+  resources :books, only: [:index, :show]
 end
